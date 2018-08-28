c65cedcbff33f0128a90df724e164aeb185c9efd
mikey <manicdeisgner@gmail.com>
2018-08-28 18:28:48 -0400

    add_task function added.

--- inc/functions.php
+++ inc/functions.php
@@ -18,12 +18,12 @@ function get_task_list() {
     $sql = 'SELECT t.task_id as task_id, t.title as title, t.date as dates,
             t.time as time , p.title as projects FROM tasks as t 
              JOIN projects as p ON  
-             p.projects_it = t.project_id';
+             p.project_id = t.project_id';
 
     try {
-
+        return $db->query($sql);
     } catch (Exception $e) {
-        echo 'Error' . $e->getMessage() . "<br>";
+        echo 'Error ' . $e->getMessage() . "<br>";
         return array();
     }
 
@@ -38,6 +38,26 @@ function add_project($title, $category) {
         $results = $db->prepare($sql);
         $results->bindParam(1, $title, PDO::PARAM_STR);
         $results->bindParam(2, $category, PDO::PARAM_STR);
+        $results->execute();
+    } catch (Exception $e) {
+        echo 'Error '. $e->getMessage();
+        return false;
+    }
+    return true;
+
+}
+
+function add_task($project_id, $title, $date, $time) {
+    include 'connection.php';
+
+    $sql = 'INSERT INTO tasks(project_id, title, date, time) VALUES (?, ?, ?, ?)';
+
+    try {
+        $results = $db->prepare($sql);
+        $results->bindParam(1, $project_id, PDO::PARAM_INT);
+        $results->bindParam(2, $title, PDO::PARAM_STR);
+        $results->bindParam(3, $date, PDO::PARAM_STR);
+        $results->bindParam(4, $time, PDO::PARAM_INT);
         $results->execute();
     } catch (Exception $e) {
         echo 'Error '. $e->getMessage();
