cb0ffe230d04d333ddee03828f419926e1f0ba9e
michael williams <manicdesigner@gmail.com>
2018-08-29 00:35:25 -0400

    strings leave if you miss the category, the month is checked for the most part.

--- task.php
+++ task.php
@@ -4,16 +4,30 @@ require 'inc/functions.php';
 $pageTitle = "Task | Time Tracker";
 $page = "tasks";
 
+$project_id = $title = $date = $time = '';
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $project_id = trim(filter_input(INPUT_POST, 'project_id', FILTER_SANITIZE_NUMBER_INT));
     $title = trim(filter_input(INPUT_POST, 'title', FILTER_SANITIZE_STRING));
     $date = trim(filter_input(INPUT_POST, 'date', FILTER_SANITIZE_STRING));
     $time = trim(filter_input(INPUT_POST, 'time', FILTER_SANITIZE_NUMBER_INT));
 
+    $date_time = strftime('%Y/%m/%d', $date);
+    $date_match = explode('/', $date);
+
+    var_dump('time'. $date_time);
+
 
     if( empty($project_id) ||  empty($title) || empty($date) || empty($time) ) {
         $error_message = 'Please fill in the required fields: Project, Title, date, Time.';
-    } else {
+    } elseif (count($date_match) != 3
+            || strlen($date_match[0]) != 4
+            || strlen($date_match[1]) != 2
+            || strlen($date_match[2]) != 2)
+
+              {
+        $error_message = 'Invalid Date';
+        } else {
         if (add_task($project_id, $title, $date, $time)) {
             header('Location: task_list.php');
             exit;
@@ -45,7 +59,11 @@ include 'inc/header.php';
                                 <option value="">Select One</option>
                                 <?php
                                 foreach (get_project_list() as $item) {
-                                    echo "<option value=".$item['project_id'].">" . $item['title'] . "</option>";
+                                    echo "<option value='".$item['project_id']."'";
+                                    if ($project_id == $item['project_id']) {
+                                      echo ' selected';
+                                    }
+                                    echo ">" . $item['title'] . "</option>";
                                 }
                                 ?>
                             </select>
@@ -53,15 +71,15 @@ include 'inc/header.php';
                     </tr>
                     <tr>
                         <th><label for="title">Title<span class="required">*</span></label></th>
-                        <td><input type="text" id="title" name="title" value="" /></td>
+                        <td><input type="text" id="title" name="title" value="<?php echo htmlspecialchars($title); ?>" /></td>
                     </tr>
                     <tr>
                         <th><label for="date">Date<span class="required">*</span></label></th>
-                        <td><input type="text" id="date" name="date" value="" placeholder="yyyy/mm/dd" /></td>
+                        <td><input type="text" id="date" name="date" value="<?php echo htmlspecialchars($date); ?>" placeholder="yyyy/mm/dd" /></td>
                     </tr>
                     <tr>
                         <th><label for="time">Time<span class="required">*</span></label></th>
-                        <td><input type="text" id="time" name="time" value="" /> minutes</td>
+                        <td><input type="text" id="time" name="time" value="<?php echo htmlspecialchars($time); ?>" /> minutes</td>
                     </tr>
                 </table>
                 <input class="button button--primary button--topic-php" type="submit" value="Submit" />
